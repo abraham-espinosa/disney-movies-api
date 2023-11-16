141cd4ca3a60b3f6f3ed8847de1ddca8e7ece23d
abraham-1 <esp19005@byui.edu>
2023-11-16 12:09:10 -0600

    Create movies api

---
+++ src/main/java/org/abrahameg/movies/MoviesApplication.java
@@ -0,0 +1,18 @@
+package org.abrahameg.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MoviesApplication {
+ 
+	public static void main(String[] args) {SpringApplication.run(MoviesApplication.class, args);}
+	@GetMapping("/") // GET endpoint
+	public String apiRoute(){
+		return "Hello World";
+	}
+
+}
