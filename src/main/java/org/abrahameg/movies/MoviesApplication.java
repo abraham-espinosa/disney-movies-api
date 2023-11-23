7340f9147152b804a9dbe42258d12bf0b00e5e2a
abraham-1 <esp19005@byui.edu>
2023-11-23 09:53:37 -0600

    Update MoviesApplication.java

--- src/main/java/org/abrahameg/movies/MoviesApplication.java
+++ src/main/java/org/abrahameg/movies/MoviesApplication.java
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class MoviesApplication {
- 
+ 	public String homeMessage = "API Disney Movies\n\n" +
+			"Main URL: https://abrahameg-movies.onrender.com\n" +
+			"GET all movies: '/api/v1/movies'\n" +
+			"GET movie by imdbId:  '/api/v1/movies/{imdbId}'\n" +
+			"POST new review: '/api/v1/reviews'";
 	public static void main(String[] args) {SpringApplication.run(MoviesApplication.class, args);}
 	@GetMapping("/") // GET endpoint
 	public String apiRoute(){
-		return "Hello World";
+		return homeMessage;
 	}
 
 }
